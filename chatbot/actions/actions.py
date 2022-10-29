c3a81faba456c1b09a648f21bc9a8705c99bb09c
Luigi Scovotto <luigiscovotto@libero.it>
2022-10-29 15:50:26 +0200

    update
    -fix the problem on the form and start to make the file action.py.
    -update with the module trained
    -code validate

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -25,3 +25,31 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionSubmit(Action):
+
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        hour = tracker.get_slot("hour")
+        category = tracker.get_slot("category")
+        task = tracker.get_slot("task")
+
+        print("\nHour:",hour)
+        print("Category:",category)
+        print("Task:",task)
+        
+        dispatcher.utter_message(text=hour)
+        dispatcher.utter_message(text=category) 
+        dispatcher.utter_message(text=task) 
+
+        return []
