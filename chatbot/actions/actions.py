64b03adff80b3e2813a6a5fb617149fd4a561d78
Luigi Scovotto <luigiscovotto@libero.it>
2022-10-29 17:35:48 +0200

    update

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -30,16 +30,17 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
 class ActionSubmit(Action):
-
+    
     def name(self) -> Text:
         return "action_submit"
-
+    
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
+
         hour = tracker.get_slot("hour")
         category = tracker.get_slot("category")
         task = tracker.get_slot("task")
@@ -49,7 +50,8 @@ class ActionSubmit(Action):
         print("Task:",task)
         
         dispatcher.utter_message(text=hour)
-        dispatcher.utter_message(text=category) 
-        dispatcher.utter_message(text=task) 
+        dispatcher.utter_message(text=category)
+        dispatcher.utter_message(text=task)
+        dispatcher.utter_message(text=f"This is the address: {hour} {category} {task}") 
 
-        return []
+        return [SlotSet("hour", hour),SlotSet("category", category),SlotSet("category", category)]
